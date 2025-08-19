3a5fe2c981dd55608646043c094a587e8baec72b
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2025-08-19 14:03:24 -0300

    commit

--- blog/next.config.ts
+++ blog/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['159-112-182-249.traefik.me'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '159-112-182-249.traefik.me',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      }
+    ]
+  }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
